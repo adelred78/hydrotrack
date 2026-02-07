4bbdd8e40d89e6cacb88a5ce94d3d4a8b8dccd79
Adel R <redjemitechkraft@gmail.com>
2026-02-07 19:02:56 +0100

    petite_modif

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = 'https://ullusqudzgaqkblsrsoq.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_oNwn7Mn-zQtj-rvM62J6TA_fCIfeCrX';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-console.log('Supabase connecté :', supabase);
+console.log('Supabase connecté :', supabaseClient);
